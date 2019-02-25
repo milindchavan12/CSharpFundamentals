20c07e375832b7fcd9a9503f685b91c1a22e8ef9
Milind Chavan <milind.chavan@live.com>
2019-02-25 14:51:06 +0100

    With Explicit Interface implementation

---
+++ Interfaces/Example-ExplicitImplementation/ExplicitImplementation/OddGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExplicitImplementation
+{
+    public class OddGenerator : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            int i = 1;
+            yield return i;
+            while(true){
+                i += 2;
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Interfaces/Example-ExplicitImplementation/ExplicitImplementation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExplicitImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd Number");
+
+            var oddnumber = new OddGenerator();
+
+            foreach (var number in oddnumber)
+            {
+                if (number > 50)
+                    break;
+
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
